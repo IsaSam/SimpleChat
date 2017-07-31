ec5f52fe3fabdd8c8ccc9a40d540afa270019c65
IsaSam <isaacsamuel27@gmail.com>
2017-07-31 00:56:20 -0400

    commit 2

---
+++ app/src/main/java/com/codepath/isaac/simplechat/ChatApplication.java
@@ -0,0 +1,36 @@
+package com.codepath.isaac.simplechat;
+
+import android.app.Application;
+
+import com.parse.Parse;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+
+/**
+ * Created by isaac_000 on 7/29/2017.
+ */
+public class ChatApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Use for monitoring Parse network traffic
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor();
+        // Can be Level.BASIC, Level.HEADERS, or Level.BODY
+        httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        builder.networkInterceptors().add(httpLoggingInterceptor);
+
+        // set applicationId and server based on the values in the Heroku settings.
+        // any network interceptors must be added with the configuration Builder given this syntax
+       /* Parse.initialize(new Parse.Configuration.Builder(this)
+        .applicationId("simplechat-client")
+        .clientBuilder(builder)
+        .server("https://codepath-chat-lab.herokuapp.com/parse/").build());*/
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("simplechat-client") // should correspond to APP_ID env variable
+                .clientBuilder(builder)
+                .server("https://codepath-chat-lab.herokuapp.com/parse/").build());
+    }
+}
